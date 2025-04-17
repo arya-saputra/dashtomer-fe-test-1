740301f0523b00b9b9350789202c97bd5dc22765
Arya Saputra <m.aryayudha.s@gmail.com>
2025-04-17 07:27:55 +0700

    fixed menu in mobile

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,17 @@
 window.addEventListener("load", () => {
     const menuIcon = document.querySelectorAll(".menu-icon")[0];
     const menu = document.querySelectorAll(".menu")[0];
+    const menuInp = document.querySelectorAll(".menu-btn")[0];
+    const navLink = document.querySelectorAll(".menu li");
 
     menuIcon.addEventListener("click", () => {
         menu.classList.toggle("expanded");
+    });
+    
+    navLink.forEach((link, key) => {
+        link.addEventListener("click", () => {
+            menuInp.checked = false;
+            menu.classList.remove("expanded");
+        });
     })
 })
